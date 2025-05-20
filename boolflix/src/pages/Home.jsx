76146b326e56c71aec500dccbbac7f1683643503
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-20 14:29:38 +0200

    [UPD] aggiunte serie tv, ridimensionate le bandiere

--- boolflix/src/pages/Home.jsx
+++ boolflix/src/pages/Home.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react'
 import Navbar from '../components/NavBar'
+import { getFlag } from '../utils/flags'
 
 function Home() {
     const [films, setFilms] = useState([])
 
     function handleSearch(query) {
         const apiKey = '81beca33f6cd2dbe2854cf52ddd74491'
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&language=it-IT`
+        const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&language=it-IT`
+        const tvUrl = `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${query}&language=it-IT`
 
-        fetch(url)
-            .then((res) => res.json())
-            .then((data) => {
-                setFilms(data.results)
+        Promise.all([
+            window.fetch(movieUrl).then(res => res.json()),
+            window.fetch(tvUrl).then(res => res.json())
+        ])
+            .then(([movieData, tvData]) => {
+                const movies = movieData.results.map(movie => ({ ...movie, type: 'movie' }))
+                const tvShows = tvData.results.map(tv => ({ ...tv, type: 'tv' }))
+                setFilms([...movies, ...tvShows])
             })
-            .catch((err) => {
-                console.log('Errore nella fetch:', err)
-            })
+            .catch(err => console.log('Errore fetch:', err))
     }
 
     return (
@@ -23,15 +27,26 @@ function Home() {
             <Navbar onSearch={handleSearch} />
 
             <div>
-                {films.map((film) => (
-                    <div key={film.id}>
-                        <h3>{film.title}</h3>
-                        <p>Titolo originale: {film.original_title}</p>
-                        <p>Lingua: {film.original_language}</p>
-                        <p>Voto: {film.vote_average}</p>
-                        <hr />
-                    </div>
-                ))}
+                {films.map((item) => {
+                    const flagImg = getFlag(item.original_language)
+
+                    return (
+                        <div key={item.id}>
+                            <h3>{item.type === 'movie' ? item.title : item.name} ({item.type.toUpperCase()})</h3>
+                            <p>Titolo originale: {item.original_title || item.original_name}</p>
+                            <p>
+                                Lingua:
+                                {flagImg ? (
+                                    <img className='flag-img' src={flagImg} alt={item.original_language} />
+                                ) : (
+                                    item.original_language
+                                )}
+                            </p>
+                            <p>Voto: {item.vote_average}</p>
+                            <hr />
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
